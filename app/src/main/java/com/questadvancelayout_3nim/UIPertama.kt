30204a48939b88d4a57c758e8c44b4b649f26572
Ezra Abrari <ezraabrari@gmail.com>
2025-10-23 11:21:48 +0700

    Membuat Folder baru untuk membuat UI APLIKASI

---
+++ app/src/main/java/com/questadvancelayout_3nim/UIPertama.kt
@@ -0,0 +1,7 @@
+package com.questadvancelayout_3nim
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
