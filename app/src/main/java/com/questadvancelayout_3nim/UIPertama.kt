9511f94844c97bd88d55410bf997242e6ac1f588
Ezra Abrari <ezraabrari@gmail.com>
2025-10-23 11:35:32 +0700

    Membuat colom dan dimodifikasikan dengan di padding dengan 100 dp, dan mengatur nya ke horizontal

--- app/src/main/java/com/questadvancelayout_3nim/UIPertama.kt
+++ app/src/main/java/com/questadvancelayout_3nim/UIPertama.kt
@@ -1,11 +1,19 @@
 package com.questadvancelayout_3nim
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun ActivitasPertama(modifier: Modifier) {
+    Column(modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize(),
+        HorizontalAlignment = Alignment.CenterHorizontally
+    ){
 
+    }
 }
