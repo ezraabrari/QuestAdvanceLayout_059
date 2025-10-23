86a824f5431c3d35b22ab783fd1f0ea6ad08ecc6
Ezra Abrari <ezraabrari@gmail.com>
2025-10-23 11:40:32 +0700

    Memanggil Text yaitu Prodi pada resource dan nge bold tulisan nya dan mengatur kebesaran fontnya

--- app/src/main/java/com/questadvancelayout_3nim/UIPertama.kt
+++ app/src/main/java/com/questadvancelayout_3nim/UIPertama.kt
@@ -3,17 +3,23 @@ package com.questadvancelayout_3nim
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ActivitasPertama(modifier: Modifier) {
     Column(modifier = Modifier.padding(top=100.dp)
         .fillMaxSize(),
         HorizontalAlignment = Alignment.CenterHorizontally
-    ){
-
+    ) {
+        Text(stringResource( R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold)
     }
 }
